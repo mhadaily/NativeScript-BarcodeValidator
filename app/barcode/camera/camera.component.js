a62ffb23fe845d89a7d46ef325463aca61b7fa62
Majid <mhadaily@gmail.com>
2017-01-11 00:04:55 +0800

    Add Camera Component and ready to Scan via Barcode Validator Service

--- app/barcode/camera/camera.component.js
+++ app/barcode/camera/camera.component.js
@@ -1,35 +1,68 @@
 "use strict";
 var core_1 = require("@angular/core");
+var camera = require("nativescript-camera");
 var nativescript_barcodescanner_1 = require("nativescript-barcodescanner");
 var CameraComponent = (function () {
     function CameraComponent(barcodeScanner) {
         this.barcodeScanner = barcodeScanner;
+        this.cameraAvailable = false;
+        this.saveToGallery = true;
+        this.keepAspectRatio = true;
+        this.width = 300;
+        this.height = 300;
     }
-    CameraComponent.prototype.onClick = function () {
-        this.barcodeScanner.available().then(function (avail) {
-            console.log("Available? " + avail);
+    CameraComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.barcodeScanner.available().then(function (res) {
+            console.log("Is camera hardware available: " + res);
+            _this.cameraAvailable = true;
+        }).catch(function (e) {
+            alert('Camera is not Available ' + e);
         });
-        this.barcodeScanner.hasCameraPermission().then(function (granted) {
-            console.log("Has Camera Permission? " + granted);
+    };
+    CameraComponent.prototype.onTakePhoto = function () {
+        var _this = this;
+        var options = {
+            width: this.width,
+            height: this.height,
+            keepAspectRatio: this.keepAspectRatio,
+            saveToGallery: this.saveToGallery
+        };
+        camera.takePicture(options)
+            .then(function (imageAsset) {
+            _this.imageTaken = imageAsset;
+            console.log("Size: " + imageAsset.options.width + "x" + imageAsset.options.height);
+        }).catch(function (err) {
+            console.log(err.message);
         });
+    };
+    CameraComponent.prototype.onRequestPermissions = function () {
         this.barcodeScanner.requestCameraPermission().then(function () {
             console.log("Camera permission requested");
         });
-        this.barcodeScanner.scan({
-            //Android only "DATA_MATRIX, CODABAR, MAXICODE, ITF, RSS_14, UPC_A]
-            formats: "CODE_39, CODE_93, CODE_128, EAN_8, EAN_13, QR_CODE, UPC_E, AZTEC, PDF_417",
-            showFlipCameraButton: true,
-            preferFrontCamera: false,
-            showTorchButton: true,
-            torchOn: false,
-        }).then(function (result) {
-            alert({
-                title: "Scan result",
-                message: "Format: " + result.format + ",\nValue: " + result.text,
-                okButtonText: "OK"
+    };
+    CameraComponent.prototype.onScan = function () {
+        var _this = this;
+        this.barcodeScanner.hasCameraPermission().then(function () {
+            _this.barcodeScanner.scan({
+                //Android only "DATA_MATRIX, CODABAR, MAXICODE, ITF, RSS_14, UPC_A]
+                formats: "CODE_39, CODE_93, CODE_128, EAN_8, EAN_13, QR_CODE, UPC_E, AZTEC, PDF_417",
+                showFlipCameraButton: true,
+                preferFrontCamera: false,
+                showTorchButton: true,
+                torchOn: false,
+            }).then(function (result) {
+                alert({
+                    title: "Scan result",
+                    message: "Format: " + result.format + ",\nValue: " + result.text,
+                    okButtonText: "OK"
+                });
+            }, function (errorMessage) {
+                console.log("No scan. " + errorMessage);
             });
-        }, function (errorMessage) {
-            console.log("No scan. " + errorMessage);
+        }).catch(function (e) {
+            console.log(e);
+            _this.onRequestPermissions();
         });
     };
     CameraComponent = __decorate([
@@ -43,4 +76,3 @@ var CameraComponent = (function () {
     return CameraComponent;
 }());
 exports.CameraComponent = CameraComponent;
-//# sourceMappingURL=camera.component.js.map
--- app/barcode/camera/camera.component.ts
+++ app/barcode/camera/camera.component.ts
@@ -1,49 +1,83 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as camera from "nativescript-camera";
 import { BarcodeScanner } from "nativescript-barcodescanner";
+import { ImageAsset } from "image-asset";
 
 @Component({
 	moduleId: module.id,
 	selector: "app-camera",
 	templateUrl: "camera.component.html",
 })
-export class CameraComponent {
+export class CameraComponent implements OnInit {
+	
 	constructor(private barcodeScanner: BarcodeScanner) {
 	}
 	
-	onClick() {
-		this.barcodeScanner.available().then(
-				function(avail) {
-					console.log("Available? " + avail);
+	public cameraAvailable: boolean = false;
+	public imageTaken: ImageAsset;
+	public saveToGallery: boolean = true;
+	public keepAspectRatio: boolean = true;
+	public width: number = 300;
+	public height: number = 300;
+	
+	ngOnInit() {
+		this.barcodeScanner.available().then((res) => {
+					console.log("Is camera hardware available: " + res);
+					this.cameraAvailable = true;
 				}
-		);
+		).catch((e) => {
+			alert('Camera is not Available ' + e);
+		});
+	}
+	
+	onTakePhoto() {
+		let options = {
+			width: this.width,
+			height: this.height,
+			keepAspectRatio: this.keepAspectRatio,
+			saveToGallery: this.saveToGallery
+		};
 		
-		this.barcodeScanner.hasCameraPermission().then(
-				function(granted) {
-					console.log("Has Camera Permission? " + granted);
-				}
-		);
-		
+		camera.takePicture(options)
+		      .then(imageAsset => {
+			      this.imageTaken = imageAsset;
+			      console.log("Size: " + imageAsset.options.width + "x" + imageAsset.options.height);
+		      }).catch(err => {
+			console.log(err.message);
+		});
+	}
+	
+	onRequestPermissions() {
 		this.barcodeScanner.requestCameraPermission().then(
 				function() {
 					console.log("Camera permission requested");
 				}
 		);
-		this.barcodeScanner.scan({
-			//Android only "DATA_MATRIX, CODABAR, MAXICODE, ITF, RSS_14, UPC_A]
-			formats: "CODE_39, CODE_93, CODE_128, EAN_8, EAN_13, QR_CODE, UPC_E, AZTEC, PDF_417",
-			showFlipCameraButton: true,
-			preferFrontCamera: false,
-			showTorchButton: true,
-			torchOn: false,
-		}).then((result) => {
-					alert({
-						title: "Scan result",
-						message: "Format: " + result.format + ",\nValue: " + result.text,
-						okButtonText: "OK"
-					});
-				}, (errorMessage) => {
-					console.log("No scan. " + errorMessage);
+	}
+	
+	onScan() {
+		this.barcodeScanner.hasCameraPermission().then(() => {
+					this.barcodeScanner.scan({
+						//Android only "DATA_MATRIX, CODABAR, MAXICODE, ITF, RSS_14, UPC_A]
+						formats: "CODE_39, CODE_93, CODE_128, EAN_8, EAN_13, QR_CODE, UPC_E, AZTEC, PDF_417",
+						showFlipCameraButton: true,
+						preferFrontCamera: false,
+						showTorchButton: true,
+						torchOn: false,
+					}).then((result) => {
+								alert({
+									title: "Scan result",
+									message: "Format: " + result.format + ",\nValue: " + result.text,
+									okButtonText: "OK"
+								});
+							}, (errorMessage) => {
+								console.log("No scan. " + errorMessage);
+							}
+					);
 				}
-		);
+		).catch((e) => {
+			console.log(e);
+			this.onRequestPermissions();
+		});
 	}
 }
